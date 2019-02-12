7104ec8c38a4375e046540bf688767b1137eb5f3
Nicolas Samaha <nicolasnsamaha@hotmail.com>
2019-02-12 17:40:31 -0500

    yo

--- wifi-bot.h
+++ wifi-bot.h
@@ -10,24 +10,12 @@ const int BACK_RIGHT_FORWARD = 3;
 const int BACK_LEFT_BACKWARD = 12;
 const int BACK_LEFT_FORWARD = 13;
 
-//RPI3 WIRINGPI PIN NUMBERS FOR LEDS
-const int BLUE = 15;
-const int RED = 16;
-const int GREEN = 1;
-
 //MOVEMENT FUNCTIONS
 int moveForward();
 int moveBackward();
 int rotateRight();
 int rotateLeft();
 void stopMove(int);
-
-//LED FUNCTIONS
-void toggleLed();
-
-//PROGRAM OPTIONS
-bool toggle;
-	
 
 //MOVEMENT RETURN VALUES (used for determining last movement call)
 const int FORWARD_MOVE = 1;
